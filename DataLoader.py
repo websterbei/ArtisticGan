353c945e2ef0375d9f18566b2333c7551271b29e
Webster Bei Yijie <yijie.bei@duke.edu>
2019-02-07 10:46:43 -0500

    clean up

--- DataLoader.py
+++ DataLoader.py
@@ -6,8 +6,8 @@ class DataLoader(object):
         image_files = [os.path.join(path, x) for x in os.listdir(path) if x.endswith('.jpg')]
         self.images = tf.data.Dataset.from_tensor_slices(image_files)
 
-    def initialize_dataset(self):
+    def initialize_dataset(self, batch_size=5):
         def input_parser(image_path):
             img_file = tf.read_file(image_path)
             return tf.image.convert_image_dtype(tf.image.decode_image(img_file, channels=3), dtype=tf.float32)
-        return self.images.map(input_parser).repeat(100000).batch(5)
+        return self.images.map(input_parser).repeat(100000).batch(batch_size)
--- Train.py
+++ Train.py
@@ -10,7 +10,6 @@ output_dim = 128
 batch_size = 5
 
 input_dir = './img_resized'
-#output_dir = '/media/webster/Data/img_output'
 output_dir = './img_output'
 max_step = 50000
 
@@ -38,7 +37,7 @@ def get_ops(geneator, discriminator):
 
 def get_data():
     data_loader = DataLoader(input_dir)
-    dataset = data_loader.initialize_dataset()
+    dataset = data_loader.initialize_dataset(batch_size=batch_size)
     iterator = dataset.make_one_shot_iterator()
     images = iterator.get_next()
     return images
@@ -50,6 +49,34 @@ with tf.device('GPU:0'):
     G_optim, D_optim, clip_op = get_ops(generator, discriminator)
     images = get_data()
 
+def train_generator(sess):
+    z = np.random.uniform(0,1,(batch_size, noise_dim))
+    image_batch = sess.run(images)
+    gl, _ = sess.run([G_loss, G_optim], feed_dict={noise:z, input_image:image_batch})
+    return gl
+
+def train_discriminator(sess):
+    z = np.random.uniform(0,1,(batch_size, noise_dim))
+    image_batch = sess.run(images)
+    dl, _, _= sess.run([D_loss, D_optim, clip_op], feed_dict={noise:z, input_image:image_batch})
+    return dl
+
+def generate_image(sess):
+    z = np.random.uniform(0,1,(batch_size, noise_dim))
+    g_image = sess.run(generated_image, {noise:z})[0,:,:,:]
+    im.io.imsave(os.path.join(output_dir, '{}.jpg'.format(i)), g_image)
+
+def update_train_control_variables(g_loss, d_loss, k_tot):
+    d_loss_mean = sum(d_loss)/len(d_loss)
+    g_loss_mean = sum(g_loss)/len(g_loss)
+    del d_loss
+    del g_loss
+    print("Discriminator Loss: {},    Generator Loss: {}".format(d_loss_mean, g_loss_mean))
+    k_d = min(k_tot, max(1, int(d_loss_mean/(d_loss_mean + g_loss_mean)*k_tot)))
+    k_g = min(k_tot, max(1, int(g_loss_mean/(d_loss_mean + g_loss_mean)*k_tot)))
+    return k_g, k_d
+
+# Training control variables
 k_d = 5
 k_g = 5
 k_tot = 10
@@ -57,34 +84,17 @@ k_tot = 10
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     for i in range(200):
-        z = np.random.uniform(0,1,(batch_size, noise_dim))
-        image_batch = sess.run(images)
-        #dl, _, _= sess.run([D_loss, D_optim, clip_op], feed_dict={noise:z, input_image:image_batch})
-        dl, _ = sess.run([D_loss, D_optim], feed_dict={noise:z, input_image:image_batch})
+        dl = train_discriminator(sess)
         print("Discriminator Loss: {}".format(dl))
     for i in range(max_step):
         d_loss = []
         g_loss = []
         for _ in range(k_d):
-            z = np.random.uniform(0,1,(batch_size, noise_dim))
-            image_batch = sess.run(images)
-            #dl, _, _ = sess.run([D_loss, D_optim, clip_op], feed_dict={noise:z, input_image:image_batch})
-            dl, _ = sess.run([D_loss, D_optim], feed_dict={noise:z, input_image:image_batch})
+            dl = train_discriminator(sess)
             d_loss.append(dl)
         for _ in range(k_g):
-            z = np.random.uniform(0,1,(batch_size, noise_dim))
-            image_batch = sess.run(images)
-            gl, _ = sess.run([G_loss, G_optim], feed_dict={noise:z, input_image:image_batch})
+            gl = train_generator(sess)
             g_loss.append(gl)
-        d_loss_mean = sum(d_loss)/len(d_loss)
-        g_loss_mean = sum(g_loss)/len(g_loss)
-        del d_loss
-        del g_loss
-        print("Discriminator Loss: {},    Generator Loss: {}".format(d_loss_mean, g_loss_mean))
-        k_d = min(k_tot, max(1, int(d_loss_mean/(d_loss_mean + g_loss_mean)*k_tot)))
-        k_g = min(k_tot, max(1, int(g_loss_mean/(d_loss_mean + g_loss_mean)*k_tot)))
-        print("kd: {} kg:{}".format(k_d, k_g))
+        k_g, k_d = update_train_control_variables(g_loss, d_loss, k_tot)
         if i%10==0:
-            z = np.random.uniform(0,1,(batch_size, noise_dim))
-            g_image = sess.run(generated_image, {noise:z})[0,:,:,:]
-            im.io.imsave(os.path.join(output_dir, '{}.jpg'.format(i)), g_image)
+            generate_image(sess)
